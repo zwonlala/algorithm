fd4f095777252f984ee67e39795de221f9f235e6
jiwon_lala <s26788761@naver.com>
2019-11-08 23:30:04 +0900

    191108 solved problems

--- boj11052.cpp
+++ boj11052.cpp
@@ -4,20 +4,30 @@
 
 #include "iostream"
 
+#define MAX(X,Y) {(X)>(Y)? (X) : (Y)}
+
 using namespace std;
 
 int main() {
     int T;
-    int input[1001];
-    int max;
+    int input[1001], max[1001];
+    int temp;
     cin >> T;
 
     for (int i=1; i<=T; i++) {
         cin >> input[i];
+        max[i] = input[i];
     }
 
-    max = input[1];
     for (int i=2; i<=T; i++) {
-        
+        int value = max[i];
+
+        for (int j=1; j<i; j++) {
+            temp = max[j] + input[i-j];
+            value = MAX(value, temp);
+        }
+        max[i] = value;
     }
+
+    cout << max[T] << endl;
 }
