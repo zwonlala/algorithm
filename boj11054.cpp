da5c3dfde64b4a25aea108fca9e1e61b408a5862
jiwon_lala <s26788761@naver.com>
2019-11-23 23:59:30 +0900

    191123 solved problems

---
+++ boj11054.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 송지원 on 2019-11-23.
+//
+
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    int D[1001][2], A[1001];
+    int N, ANS, temp, i, j, k;
+
+    cin >> N;
+
+    for (i=0; i<N; i++) {
+        cin >> A[i+1];
+    }
+
+    for (i=1; i<=N; i++) {
+        for (j=1; j<i; j++){
+            
+        }
+    }
+}
