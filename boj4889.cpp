bf986eba55d8033578c6654d96b2ee4af547f5ce
jiwon_lala <s26788761@naver.com>
2020-08-15 16:19:53 +0900

    200815 solved problems

--- boj4889.cpp
+++ boj4889.cpp
@@ -4,24 +4,46 @@
 
 #include <iostream>
 #include <stack>
-#include <string.h>
+#include <string>
 
 using namespace std;
+
 string str;
 int N;
-int strlen;
+int strLen;
+int CNT;
+
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     while (true) {
+        stack<char> S;
         N++;
+        CNT = 0;
         cin >> str;
         if (str[0] == '-') {
             break;
         }
-        strlen = str.size();
+        strLen = str.size();
+        for (int i=0; i<strLen; i++) {
+            char ch = str[i];
+            if (ch == '{'){
+                S.push('{');
+            }
+            else {
+                if (S.size() == 0) {
+                    CNT ++;
+                    S.push('{');
+                }
+                else S.pop();
+            }
+        }
+        if (S.size() > 0) {
+            CNT += (S.size())/2;
+        }
+        cout << N <<". " << CNT << "\n";
     }
     return 0;
 }
